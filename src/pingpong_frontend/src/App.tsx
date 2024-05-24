3698da1a915f660d52730a09a8316a35a93db188
krunal karena <krunalkarena26@gmail.com>
2024-05-24 12:00:05 +0530

    sample code added

---
+++ src/pingpong_frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import PingPong from "./Components/PingPong";
+import Header from "./Components/Header";
+
+const App = () => {
+
+  return (
+    <div className="bg-gray-900 text-gray-300">
+    <Header />
+    <PingPong/>
+    </div>
+  );
+};
+
+export default App;
